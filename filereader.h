ea293a998a5fa8080f0c2b849e29eb7a884e601e
DreaMTH <d.dimcard@gmail.com>
2023-04-02 20:58:49 +0300

    Base realization of reader class and methods.

--- filereader.h
+++ filereader.h
@@ -2,10 +2,25 @@
 #define FILEREADER_H
 
 
+#include <string>
+#include <QVector>
 class FileReader
 {
 public:
     FileReader();
+    FileReader(std::string path, QVector<std::string> input);
+    FileReader(std::string path, QVector<std::string> &output);
+    void SetPath(std::string path);
+    std::string GetPath();
+    void SetInput(QVector<std::string> input);
+    void SetOutput(QVector<std::string> &output);
+    void ReadFromFile();
+    ~FileReader();
+private:
+    const std::string defaultErrorPath = "debugInformation.txt";
+    std::string _path;
+    QVector<std::string> *output;
+    QVector<std::string> input;
 };
 
 #endif // FILEREADER_H
